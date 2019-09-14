fbd7d5e6165f39452d37ed711d731c896e1241cc
Saad Masood <saad@findhotel.net>
2019-09-15 01:31:31 +0200

    upgrade for 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,20 @@
 output "ecs_cluster_arn" {
   description = "The ARN of the ECS cluster."
-  value       = "${aws_ecs_cluster.main.arn}"
+  value       = aws_ecs_cluster.main.arn
 }
 
 output "ecs_cluster_name" {
   description = "The name of the ECS cluster."
-  value       = "${aws_ecs_cluster.main.name}"
+  value       = aws_ecs_cluster.main.name
 }
 
 output "ecs_instance_role" {
   description = "The name of the ECS instance role."
-  value       = "${aws_iam_role.ecs_instance_role.name}"
+  value       = aws_iam_role.ecs_instance_role.name
 }
 
 output "asg_security_group" {
   description = "The name of the Security group attached to the ASG instances."
-  value       = "${aws_security_group.main.id}"
+  value       = aws_security_group.main.id
 }
+
